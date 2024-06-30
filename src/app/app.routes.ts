0f03705734b43690a42f0f5d74de55d02730b1ee
Ben Angular <zdc463956923@gmail.com>
2024-06-30 19:02:29 +0800

    Sixth commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SheetComponent } from './sheet/sheet.component';
 
 export const routes: Routes = [
-    {path: '', component: SheetComponent},
-    {path:'**', component: SheetComponent}
+    // {path: '', component: SheetComponent},
+    // {path:'**', component: SheetComponent}
 ];
